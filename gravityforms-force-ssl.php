f73e8b22e20e6d52c18ce597c9b01f86d279a427
codearachnid <codearachnid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-19 02:35:15 +0000

    Tagging version 1.3.0
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-force-ssl/tags/1.3.0@992988 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gravityforms-force-ssl.php
+++ gravityforms-force-ssl.php
@@ -4,7 +4,7 @@
  * Plugin Name: Gravity Forms: Force SSL
  * Plugin URI:
  * Description: An addon to Gravity Forms to add an option to force your forms to be loaded SSL only.
- * Version: 1.2.0
+ * Version: 1.3.0
  * Author: Timothy Wood (@codearachnid)
  * Author URI: http://www.codearachnid.com
  * Author Email: tim@imaginesimplicity.com
@@ -74,6 +74,9 @@ if ( !class_exists( 'gf_force_ssl' ) ) {
 		}
 
 		function force_ssl( $post_id = null ){
+			if( is_admin() )
+				return;
+			
 			$post_id = empty( $post_id ) ? get_the_ID() : $post_id;
 			$goto = str_replace( 'http://', 'https://', get_permalink( $post_id ) );
 			if( !empty( $_GET ) ) {
